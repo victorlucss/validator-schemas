51b98dfacfe0967161c3ad93116bf85393c020a8
Victor Lucas <victorlucas@alu.ufc.br>
2020-02-24 07:19:41 -0300

    Correction of paths and module export

---
+++ src/validate.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const validate = {
+    isString: (val) => typeof val === 'string',
+    isInteger: (val) => Number.isInteger(val),
+    isFloat: (val) => Number(val) % 1 !== 0 || Number.isInteger(val),
+    isBoolean: (val) => typeof val === 'boolean',
+    isTimestamp : (val) => val >= 0 && val < Date.now(),
+    isObject: (val) => typeof val === 'object',
+    hasProp: (obj, prop) => obj.hasOwnProperty(prop),
+    isValid: (val) => val !== null && val !== undefined,
+    isArray: (val) => Array.isArray(val)
+}
+
+module.exports = validate;
